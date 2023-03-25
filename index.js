591d4bd5cd3ad3cb48fad8c971f95d2993c73903
Jamie Grayson <126696913+jlagray@users.noreply.github.com>
2023-03-25 11:41:33 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    return [...cats, name]
+}
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+} 
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    var name_1= [...cats,name];
+    return name_1;
+}
+
+function prependCat(name) {
+    var name_2= [name, ...cats];
+    return name_2;
+}
+
+function removeFirstCat() {
+    return cats.slice[0, length - 1]}
+function removeFirstCat() {
+    return cats.slice(1) }
+function removeLastCat(name) {
+    const  cats = cats.pop(0, cats.length - 1);
+    return cats;
+}
+    
